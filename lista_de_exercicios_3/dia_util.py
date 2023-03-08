f7b7cb8e95269052719fc17b10fb777559852ce6
Péricles Andrade Feitoza <periclesanfe@gmail.com>
2023-03-08 14:51:19 -0300

    correções questão cpf e dias_da_semana

--- lista_de_exercicios_3/dia_util.py
+++ lista_de_exercicios_3/dia_util.py
@@ -1,5 +1,5 @@
-dias_da_semana = {1: 'Domingo', 2: 'Segunda', 3: 'Terça',
-                  4: 'Quarta', 5: 'Quinta', 6: 'Sexta', 7: 'Sábado'}
+dias = {1: 'Domingo', 2: 'Segunda', 3: 'Terça',
+        4: 'Quarta', 5: 'Quinta', 6: 'Sexta', 7: 'Sábado'}
 dia = int(input('Digite um número de 1 a 7: '))
-print('{}-Feira é dia util'.format(dias_da_semana[dia])) if dia in range(
-    2, 6) else print('O dia {} é um dia de final de semana'.format(dias_da_semana[dia]))
+print('{}-Feira é dia util'.format(dias[dia])) if dia in range(
+    2, 6) else print('O dia {} é um dia de final de semana'.format(dias[dia]))
--- lista_de_exercicios_3/valiar_cpf.py
+++ lista_de_exercicios_3/valiar_cpf.py
@@ -1,58 +1,3 @@
-# 4) Crie um programa para validar CPF (o usuário informa os onze dígitos do documento e ele
-# informa se o número informado é válido ou não). Um CPF é composto de 11 dígitos, em que os
-# dois últimos são os dígitos verificadores. Para ser válido, o primeiro e o segundo dígitos do
-# CPF informado devem ser iguais a determinados valores, como descrito abaixo.
-# Para validação do CPF da pessoa, utilize a seguinte estratégia:
-
-# Passo 1:
-# Multiplique o 1° dígito do CPF por 10
-# Multiplique o 2° dígito do CPF por 9
-# Multiplique o 3° dígito do CPF por 8
-# Multiplique o 4° dígito do CPF por 7
-# Multiplique o 5° dígito do CPF por 6
-# Multiplique o 6° dígito do CPF por 5
-# Multiplique o 7° dígito do CPF por 4
-# Multiplique o 8° dígito do CPF por 3
-# Multiplique o 9° dígito do CPF por 2
-
-# Passo 2: Some os resultados das multiplicações anteriores
-
-# Passo 3: Encontre o resto da divisão da soma anterior por 11
-
-# Passo 4: Subtraia o resto obtido no passo anterior de 11
-
-# Passo 5: Se o resultado da subtração for menor que 10, compare-o com o 10° dígito do CPF, se não for, compare-o com zero.
-# Se a comparação for diferente, o CPF é inválido e os passos seguintes podem ser ignorados.
-
-# Passo 6:
-# Multiplique o 1° dígito do CPF por 11
-# Multiplique o 2° dígito do CPF por 10
-# Multiplique o 3° dígito do CPF por 9
-# Multiplique o 4° dígito do CPF por 8
-# Multiplique o 5° dígito do CPF por 7
-# Multiplique o 6° dígito do CPF por 6
-# Multiplique o 7° dígito do CPF por 5
-# Multiplique o 8° dígito do CPF por 4
-# Multiplique o 9° dígito do CPF por 3
-# Multiplique o 10° dígito do CPF por 2
-
-# Passo 7: Some os resultados das multiplicações anteriores do passo anterior
-
-# Passo 8: Encontre o resto da divisão da soma do passo anterior por 11
-
-# Passo 9: Subtraia o resto obtido no passo anterior de 11
-
-# Passo 10: Se o resultado da subtração for menor que 10, compare-o com o 11° dígito do CPF 8=8;
-# se não for, compare-o com zero. Se a comparação for diferente, o CPF é inválido. Se não for, o
-# CPF é válido.
-
-# Exemplo: 111.222.333-44 é válido?
-# Soma1 = 1*10 + 1*9 + 1*8 +2*7 +2*6 + 2*5 + 3*4 + 3*3 + 3*2 = 90
-# Digito1 = 11 – (soma % 11) = 11 – 2 = 9
-# Soma2 = 1*11 + 1*10 + 1*9 + 2*8 + 2*7 + 2*6 + 3*5 + 3*4 + 3*3 + 9*2 = 126
-# Digito2 = 11 – 5 = 6
-# 111.222.333-44 não é válido! Para ser válido, deveria ser 111.222.333-96.
-
 def calculo(cpf, multi, digito_final):
     soma = 0
     for i in range(0, len(cpf)):
@@ -60,16 +5,11 @@ def calculo(cpf, multi, digito_final):
             n = int(cpf[i])*multi  # passo1/6
             soma += n  # passo2/7
             multi -= 1
-            print(n)
-        print(soma)
-    print(soma % 11)
     resultado = 11 - (soma % 11)  # passo3/8 e passo4/9
-    print(resultado)
     return resultado
 
 
 cpf = input('Digite o CPF: ')
-print(cpf)
 resultado1 = calculo(cpf, 10, 9)  # passo5
 while True:
     if resultado1 < 10:
