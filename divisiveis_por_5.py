74eef9148e19dc634c075b0790ffaf29af7c89a2
Péricles Andrade Feitoza <periclesanfe@gmail.com>
2023-03-08 21:41:19 -0300

    exercicios numeros perfeitos

--- divisiveis_por_5.py
+++ divisiveis_por_5.py
@@ -1,4 +1,5 @@
 # Apresente todos os números divisíveis por 5 entre 0 e 200 incluindo os dois.
 
-for i in range(0, 201, 5):
-    print(i)
+for i in range(0, 201):
+    if i % 5 == 0:
+        print(i)
---
+++ numero_perfeito.py
@@ -0,0 +1,9 @@
+# leia um numero e diga se é perfeito, um numeor é perfeito quando ele é igual
+# a soma dos seus divisores
+
+n = int(input('digite o numero: '))
+soma = 0
+for i in range(1, n):
+    if n % i == 0:
+        soma += i
+print('é perfeito') if n == soma else print('não é perfeito')
