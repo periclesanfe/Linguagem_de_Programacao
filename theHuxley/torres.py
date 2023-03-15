031d2145a046132a910db09190d88f2db9e437f1
Péricles Andrade Feitoza <periclesanfe@gmail.com>
2023-03-15 13:41:39 -0300

    problema das torres

--- theHuxley/torres.py
+++ theHuxley/torres.py
@@ -2,12 +2,16 @@
 # Entrada: A primeira linha da entrada terá um inteiro n (1 <= n <= 1000) indicando a quantidade de blocos a Entidade ganhou. Na linha seguinte, terão n inteiros h_i  (1 <= h_i <= 1000), representando o tamanho do i-ésima bloco.
 # Saida: A saída deverá conter dois inteiros t e k, representando respecitvamente o tamanho da maior torre e quantas torres ficaram no final.
 
-n = int(input())
-# use a poha do count() pra contar quantas vezes um elemento aparece na lista
-inputs = []
+n = int(input())  # numero de blocos
 
+input = input().split()  # separa em uma lista
+inputs = [int(i) for i in input]  # transforma em inteiros
 
-for i in range(0, n + 1):
-    inputs.append(int(input()))
+k = len(set(inputs))  # quantidade de torres
+t = 0
 
-contador = Counter(inputs)
+for number in inputs:
+    if inputs.count(number) > t:  # quantiadde de repeticoes de cada digito
+        t = inputs.count(number)
+
+print(t, k)
